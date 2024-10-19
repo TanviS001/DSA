f83f26eeedbbf478c7e0d626963bfda5914ef33e
Tanvi <145559687+TanviS001@users.noreply.github.com>
2024-10-19 19:56:46 +0530

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,13 +1,10 @@
-# Initialization of queue
 queue = []
 elt=int(input())
 
-# Adding data elements in the queue
 for i in range(elt):
   num=int(input())
   queue.append(num)
 print(queue)
 
-# Removing data elements from the queue
 queue.pop(0)
 print(queue)
