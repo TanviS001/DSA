5e90da9119d44600b29899918761f6d7b20f290b
Tanvi <145559687+TanviS001@users.noreply.github.com>
2024-10-19 19:57:02 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,13 +1,10 @@
-# Initialization of stack
 stack = []
 elt=int(input())
 
-# Adding data elements in the stack
 for i in range(elt):
   num=int(input())
   stack.append(num)
 print(stack)
 
-# Removing data elements from the stack
 stack.pop()
 print(stack)
